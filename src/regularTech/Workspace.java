232300082e6f460e8ca36d6b1a1fa93ee36b93ad
wisemanTrap <wotrin.and@yandex.ru>
2013-10-20 16:17:17 +0600

    create login panel, add actionlisteners to login

--- src/regularTech/Workspace.java
+++ src/regularTech/Workspace.java
@@ -1,17 +1,33 @@
 package regularTech;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class Workspace extends JPanel {
+	private static int s;
+	private JPanel panel; 
+	
+	public static final int LOGIN_WINDOW = 0;
+	public static final int DESCTOP = 1;
+	
 	public Workspace(){
-		this.setBackground(Color.red);
+		setLayout(new BorderLayout());
+		
+		setMode(Workspace.LOGIN_WINDOW);
 	}
 	
-	@Override 
-	protected void paintComponent(Graphics g){
-		g.drawString("Workspace windows", 100, 100);
+	private void setMode(int mode) {
+		// TODO Auto-generated method stub
+		switch(mode){
+			case Workspace.LOGIN_WINDOW:	this.panel = new loginPanel();
+											break;
+			case Workspace.DESCTOP:			;								
+		}
+
+		this.add(this.panel, BorderLayout.CENTER);
 	}
+
 }
--- src/regularTech/loginPanel.java
+++ src/regularTech/loginPanel.java
@@ -5,11 +5,15 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import layout.TableLayout;
 
 public class loginPanel extends JPanel {
 	private JTextField loginInput;
@@ -19,9 +23,24 @@ public class loginPanel extends JPanel {
 	private static Dimension inputDim;
 	
 	public loginPanel(){
-		inputDim = new Dimension(200, 80);
+		super();
+		
+		inputDim = new Dimension(200, 40);
 		loginInput = new JTextField();
 		passInput = new JTextField();
+		
+		loginInput.setPreferredSize(inputDim);
+		passInput.setPreferredSize(inputDim);
+	
+		passInput.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				String pass = arg0.getActionCommand();
+				String login = loginInput.getText();
+				System.out.println("login: " + login + "; pass: " + pass);
+			}
+		});
 		
 		layout = new GridLayout(3, 2);
 		
@@ -29,8 +48,9 @@ public class loginPanel extends JPanel {
 		JLabel passText = new JLabel("Password:");
 		
 		add(loginText);
+		add(loginInput);
 		add(passText);
-	
+		add(passInput);
 		
 	}
 }
