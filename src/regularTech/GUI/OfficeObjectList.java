5e9926db2dca2a7ba48ee877d82048216aa3a330
Wiseman <votrin.andrey@caesber.ru>
2014-05-05 14:49:51 +0700

    Создал окно добавления отчетности

---
+++ src/regularTech/GUI/OfficeObjectList.java
@@ -0,0 +1,14 @@
+package regularTech.GUI;
+
+import javax.swing.*;
+
+/**
+ * Create by Votrin Andrey (votrin.andrey@caesber.ru).
+ * DATE: 05.05.2014
+ * TIME: 14:15
+ */
+public class OfficeObjectList extends JList{
+    public OfficeObjectList() {
+        super();
+    }
+}
---
+++ src/regularTech/GUI/SearchBar.java
@@ -0,0 +1,9 @@
+package regularTech.GUI;
+
+/**
+ * Create by Votrin Andrey (votrin.andrey@caesber.ru).
+ * DATE: 05.05.2014
+ * TIME: 14:13
+ */
+public class SearchBar {
+}
