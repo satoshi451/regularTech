315553211c4c6b64efed7e9b52fd95a5014d21d0
Wiseman <votrin.andrey@caesber.ru>
2014-05-21 11:13:51 +0700

    начал переделки в мавен0проект

--- src/SQLScripts/fillOfficeObject.sql
+++
@@ -1,9 +0,0 @@
-insert into office_oject(name, office_oject_type_id) values("Lenovo_234_12",1);
-insert into office_oject(name, office_oject_type_id) values("Samsung 554",1);
-insert into office_oject(name, office_oject_type_id) values("HP_23",1);
-insert into office_oject(name, office_oject_type_id) values("Ipple 11254",1);
-insert into office_oject(name, office_oject_type_id) values("Hitachi_23",1);
-insert into office_oject(name, office_oject_type_id) values("Lenovo_23d",1);
-insert into office_oject(name, office_oject_type_id) values("Диван красный кожаный",2);
-insert into office_oject(name, office_oject_type_id) values("Кресло желтое",2);
-insert into office_oject(name, office_oject_type_id) values("Стол кухонный",2);
--- src/SQLScripts/fillOfficeObjectType.sql
+++
@@ -1,3 +0,0 @@
-insert into office_oject_type(name) values("PC");
-insert into office_oject_type(name) values("Furniture");
-insert into office_oject_type(name) values("common");
